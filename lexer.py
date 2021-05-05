3e0c6e97517355d6e35d99cb782c00c8048861f6
Yifeiy3 <yifeiy3@andrew.cmu.edu>
2021-05-04 18:53:42 -0700

    initial attempt on parser and lexer, still have minor bugs

--- lexer.py
+++ lexer.py
@@ -3,7 +3,12 @@ import re
 
 tokens = (
     'INPUT',
+    'BOOL',
     'DEF',
+    'DEFINITION',
+    'MAPPING',
+    'PREFERENCE',
+    'SECTION',
     'STRING',
     'LPAREN',
     'RPAREN',
@@ -15,69 +20,123 @@ tokens = (
     'COMMA',
     'COLON',
     'IDENT',
+    'EQUAL',
+    'MATH',
     'NUMBER',
+    'COMMENT',
+    'LONGCOMMENT',
+    'NEWLINE'
 )
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_LBRACKET = r'\{'
-t_RBRACKET = r'\}'
-t_DOT = r'\.'
-t_COMMA = r'\,'
-t_COLON = r'\:'
 
-def t_INPUT(t):
-    r'input'
-    t.value = t.value
-    return t 
+def MyLexer():
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_DOT = r'\.'
+    t_COMMA = r'\,'
+    t_COLON = r'\:'
 
-def t_DEF(t):
-    r'def'
-    t.value = t.value
-    return t 
+    def t_INPUT(t):
+        r'input'
+        return t 
 
-def t_REQUIRED(t):
-    r'required'
-    t.value = t.value
-    return t 
+    def t_DEFINITION(t):
+        r'definition'
+        return t 
 
-def t_MULTIPLE(t):
-    r'multiple'
-    t.value = t.value
-    return t 
+    def t_MAPPING(t):
+        r'mappings'
+        return t 
 
-def t_STRING(t):
-    r'((\')([a-zA-Z0-9_,\.-\\\'\s!#$%^&\*\(\)]*)(\'))|((\")([a-zA-Z0-9_,\.-\\\'\s!#$%^&\*\(\)]*)(\"))'
-    t.value = t.value
-    return t 
-
-def t_NUMBER(t):
-    r'[0-9]+'
-    return t 
+    def t_DEF(t):
+        r'def'
+        return t 
     
-def t_IDENT(t):
-    r'[a-zA-Z0-9_.-]+'
-    t.value = t.value
-    return t 
+    def t_LBRACKET(t):
+        r'(\n*)(\s)*\{'
+        return t
 
-t_ignore = ' \n\t'
-def t_error(t):
-    print("Illegal character {0}".format(t.value[0]))
-    t.lexer.skip(1)
+    def t_RBRACKET(t):
+        r'\}'
+        return t
 
-lexer = lex.lex()
+    def t_BOOL(t):
+        r'true|false'
+        return t 
 
-data = '''
-    def installed()
-{
-	subscribe(switches, "switch.off", offhandler)
-	subscribe(location, changedLocationMode)
-	subscribe(app, timedTouch)
-}
-'''
+    def t_SECTION(t):
+        r'section'
+        return t
 
-lexer.input(data)
-while True:
-    tok = lexer.token()
-    if not tok:
-        break
-    print(tok)
+    def t_PREFERENCE(t):
+        r'preferences'
+        return t 
+
+    def t_REQUIRED(t):
+        r'required'
+        return t 
+
+    def t_MULTIPLE(t):
+        r'multiple'
+        return t 
+
+    def t_EQUAL(t):
+        r'='
+        return t
+    
+    def t_MATH(t):
+        r'\+|\-|\*|\/|<<|>>|!|\~'
+        return t
+
+    def t_STRING(t):
+        r'((\')([a-zA-Z0-9_,\.-\\\'\s!#$%^&\*\(\)]*)(\'))|((\")([a-zA-Z0-9_,\.-\\\'\s!#$%^&\*\(\)]*)(\"))'
+        return t 
+
+    def t_NUMBER(t):
+        r'[0-9]+'
+        return t 
+        
+    def t_IDENT(t):
+        r'[a-zA-Z0-9_.-]+'
+        return t 
+
+    def t_NEWLINE(t):
+        r'\n+'
+        return t
+
+    t_ignore = ' \t'
+
+    def t_COMMENT(t):
+        r'\/\/.+'
+        pass 
+
+    def t_LONGCOMMENT(t):
+        r'(\/\*)(.|\n)+(\*\/)'
+        pass 
+
+    def t_error(t):
+        print("Illegal character {0}".format(t.value[0]))
+        t.lexer.skip(1)
+
+    return lex.lex()
+
+if __name__ == '__main__':
+    # data = '''def installed()
+    #     {
+    #         subscribe(switches, "switch.off", offhandler)
+    #         subscribe(location, changedLocationMode)
+    #         subscribe(app, timedTouch)
+    #     }'''
+    data = '''
+            def appTouch(evt) {
+                attr.each{
+                    log.debug "ok. $it.name, $it.values"
+                }
+            }
+        '''
+    lexer = MyLexer()
+    lexer.input(data)
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        print(tok)
--- parsefile.py
+++ parsefile.py
@@ -1,39 +1,204 @@
 import ply.yacc as yacc 
 #token map from lexer
-from lexer import tokens 
+from lexer import tokens, MyLexer
+
+def p_program_funcDef(p):
+    '''
+        program : program NEWLINE funcDef
+                | funcDef
+    '''
+    if(len(p) == 2):
+        p[0] = (None, [p[1]])
+    else:
+        p[1][1].append(p[2])
+        p[0] = p[1]
+
+def p_program_block(p):
+    '''
+        program : program NEWLINE prefBlock
+                | prefBlock
+                | notNeededBlock
+                | program NEWLINE notNeededBlock
+    '''
+    if(len(p) == 2):
+        p[0] = ([p[1]], None)
+    else:
+        p[1][0].append(p[2])
+        p[0] = p[1]
+
+def p_notNeededBlock(p):
+    '''
+        notNeededBlock : DEFINITION LPAREN error RPAREN
+                       | MAPPING LBRACKET error RBRACKET
+    '''
+    pass 
+
+def p_funcDef(p):
+    '''funcDef : DEF IDENT LPAREN paramlist RPAREN LBRACKET stmtList RBRACKET NEWLINE
+            | DEF IDENT LPAREN RPAREN LBRACKET stmtList RBRACKET NEWLINE
+    '''
+    if(len(p) == 9):
+        p[0] = ('funcDef', p[2], p[4], p[7])
+    else:
+        p[0] = ('funcDef', p[2], [], p[6])
+
+def p_prefBlock(p):
+    'prefBlock : PREFERENCE LBRACKET sectionblocklist RBRACKET NEWLINE'
+    p[0] = ('preferences', p[3])
+
+def p_sectionblocklist(p):
+    '''sectionblocklist : sectionblock
+            | sectionblocklist NEWLINE sectionblock'''
+    if(len(p) == 4):
+        p[1].append(p[3])
+        p[0] = p[1]
+    else:
+        p[0] = [p[1]]
+
+def p_sectionblock(p):
+    'sectionblock : SECTION LPAREN STRING RPAREN LBRACKET blockparamlist RBRACKET NEWLINE'
+    p[0] = p[6]
+
+def p_blockparamlist(p):
+    '''blockparamlist : blockparamlist COMMA blockparam
+            | blockparam'''
+    if(len(p) == 4):
+        p[1].append(p[3])
+        p[0] = p[1]
+    else:
+        p[0] = [p[1]]
+
+def p_blockparam(p):
+    '''blockparam : INPUT STRING
+                | STRING
+                | MULTIPLE COLON BOOL
+                | REQUIRED COLON BOOL'''
+    if(p[1] == 'input'):
+        p[0] = ('input', p[2])
+    elif(p[1] == 'multiple'):
+        p[0] = ('multiple', str(p[3]))
+    elif(p[1] == 'required'):
+        p[0] = ('required', str(p[3]))
+    else:
+        p[0] = ('capabilities', p[1])
 
 def p_stmtList(p):
-    'stmtList: 
+    '''stmtList : stmtList NEWLINE stmt
+            | stmt 
+            | stmtList stmt''' 
+    if(len(p) == 4):
+        p[1].append(p[3])
+        p[0] = p[1]
+    elif(len(p) == 3):
+        if(p[1][-1] != None) and p[2] != None:
+            #if the last result does not come from an omitted line due to error handling,
+            #the rule for stmtList stmt does not work.
+            raise Exception(
+                    "Invalid file, can not have stmt following stmtlist. list: {0}, stmt: {1}".format(p[1], p[2])
+                )
+        p[1].append(p[2])
+        p[0] = p[1]
+    else:
+        p[0] = [p[1]]
+
 def p_stmt(p):
-    'stmt: functionCall'
+    '''stmt : functionCall
+         | functionWithObj'''
     p[0] = p[1]
 
-def p_function_call(p):
-    'functionCall: IDENT LPAREN paramlist RPAREN'
-    p[0] = ('functioncall', p[1], p[3])
+def p_stmt_error(p):
+    'stmt : error NEWLINE'
+    print("we got here with p[2]: {0}".format(p[2]))
+    pass 
+
+def p_functionCall(p):
+    '''functionCall : IDENT LPAREN paramlist RPAREN
+                | IDENT LPAREN RPAREN'''
+    if(len(p) == 5):
+        p[0] = ('functioncall', p[1], p[3])
+    else:
+        p[0] = ('functioncall', p[1], [])
+
+def p_functionCall_error(p):
+    '''functionCall : IDENT error NEWLINE
+                    | IDENT LPAREN error NEWLINE'''
+    print("we got here with p[2]: {0}".format(p[2]))
+    pass
+
+def p_functionWithObj(p):
+    'functionWithObj : IDENT DOT functionCall'
+    p[0] = ('functionWithObj', p[1], p[3])
+
+def p_functionWithObj_error(p):
+    'functionWithObj : IDENT DOT error NEWLINE'
+    print("we got here with p[2]: {0}".format(p[2]))
+    pass
 
 def p_paramlist(p):
-    '''paramlist: paramlist COMMA param
+    '''paramlist : paramlist COMMA param
                 | param'''
     if(len(p) == 4):
-        p[0] = p[1].append(p[3])
+        p[1].append(p[3])
+        p[0] = p[1]
     else:
         p[0] = [p[1]]
 
 def p_param(p):
-    '''param: identparam
+    '''param : identparam
             | strparam
             | numparam '''
     p[0] = p[1]
 
 def p_identparam(p):
-    'identparam: IDENT'
-    p[0] = ("ident", str(p[1]))
+    '''identparam : IDENT
+                | IDENT NEWLINE
+                | NEWLINE IDENT'''
+    if p[1] == '\n':
+        p[0] = ("ident", str(p[2]))
+    else:
+        p[0] = ("ident", str(p[1]))
 
 def p_strparam(p):
-    'strparam: STRING'
-    p[0] = ("string", str(p[1]))
+    '''strparam : STRING
+            | STRING NEWLINE
+            | NEWLINE STRING'''
+    if p[1] == '\n':
+        p[0] = ("string", str(p[2]))
+    else:
+        p[0] = ("string", str(p[1]))
 
 def p_numparam(p):
-    'numparam: NUMBER'
-    p[0] = ("number", str(p[1]))
+    '''numparam : NUMBER
+            | NUMBER NEWLINE
+            | NEWLINE NUMBER'''
+    if p[1] == '\n':
+        p[0] = ("number", str(p[2]))
+    else:
+        p[0] = ("number", str(p[1]))
+
+def p_error(p):
+    if not p:
+        print("end of file")
+        return 
+
+    print("Ignored token: {0}".format(p))
+    return
+
+if __name__ == '__main__':
+    # Build the parser
+    parser = yacc.yacc()
+    
+    s = '''
+            def appTouch(evt) {
+                attr.each{
+                    log.debug "ok. $it.name, $it.values"
+                }
+                switchesoff.off()
+            }
+        '''
+    # s = '''preferences {
+    #     section("Turn on...") {
+    #         input "switches", "capability.switch", multiple: true
+    #     } }'''
+    result = parser.parse(s, lexer=MyLexer())
+    print(result)
